f965f2c290c24d92cfc0beeb18424489459e9992
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2025-12-28 19:29:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, jsonify, session, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_sqlalchemy import SQLAlchemy
