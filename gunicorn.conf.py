14f48d3121fe024fe9408e8757a8b4481ef6f42b
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2025-12-28 19:44:02 +0200

    Switch from eventlet to gevent for worker class

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,8 @@
-import eventlet
-eventlet.monkey_patch()
+from gevent import monkey
+monkey.patch_all()
 
 bind = "0.0.0.0:8080"
 workers = 1
-worker_class = "eventlet"
+worker_class = "gevent"  # <--- БЫЛО "eventlet", СТАЛО "gevent"
 timeout = 120
 keepalive = 5
